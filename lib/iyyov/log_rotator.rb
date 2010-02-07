6539c0ee3f372940acc56d373a0ee2017b5970bb
David Kellum <dek-oss@gravitext.com>
2010-02-07 12:00:50 -0800

    Rotator rdoc; support known pid

--- lib/iyyov/log_rotator.rb
+++ lib/iyyov/log_rotator.rb
@@ -3,32 +3,61 @@ require 'logrotate'
 module Iyyov
   include RJack
 
+  # Support check for size and rotation of a log file.
   class LogRotator
 
+    # Full path to log file to check and rotate
+    # <String>:: path (required)
     attr_accessor :log
+
+    # Maximum log size triggering rotation
+    # <Fixnum>:: bytes (default: 256M bytes)
     attr_accessor :max_size
+
+    # Number of rotated logs in addition to active log.
+    # <Fixnum>:: rotations (default: 3)
     attr_accessor :count
+
+    # GZIP compress rotated logs?
+    # <Boolean>:: (default: true)
     attr_accessor :gzip
+
+    # The signal to use post rotation (but before gzip) requesting
+    # that the daemon reopen its logs.
+    # <String>:: (default: "HUP")
     attr_accessor :signal
+
+    # Period between subsequent checks for rotation (default: 300.0)
+    # <Float>:: seconds
     attr_accessor :check_period
 
-    # Set max_size in megabytes
+    # Process ID to signal (if known in advance/constant, i.e. 0 for
+    # this process)
+    attr_writer :pid
+
+    # mb<Fixnum>:: Set max_size in megabytes
     def max_size_mb=( mb )
       @max_size = mb * 1024 * 1024
     end
 
-    def initialize
-      @max_size = 512 * 1024 * 1024
+    def initialize( log = nil )
+      @log = log
+      max_size_mb = 256
       @count = 3
       @gzip = true
       @signal = "HUP"
       @check_period = 5 * 60.0
-      @log = nil
+      @pid = nil
+
+      yield self if block_given?
+
+      #FIXME: Validate log directory?
+      nil
     end
 
     # Check if log is over size and rotate if needed. Yield log name
     # to block just before rotating
-    def check_rotate( pid )
+    def check_rotate( pid = @pid )
       if File.exist?( log ) && File.size( log ) > max_size
         yield log if block_given?
         opts  = { :count => count, :gzip => gzip }
