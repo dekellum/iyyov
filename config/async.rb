21f666bdb05458dcf5ca3debb338355972880ade
David Kellum <dek-oss@gravitext.com>
2010-03-13 16:18:19 -0800

    Cleanup samples

---
+++ config/async.rb
@@ -0,0 +1,11 @@
+
+Iyyov.context do |c|
+  count = 0
+  log = SLF4J["hello"]
+  c.schedule_at( :name => "hello", :mode => :async, :period => 1.5 ) do
+    count += 1
+    sleep count
+    log.info "Hello after #{count}s"
+    :stop unless count <= 10 # Avoid killing ourselves.
+  end
+end
